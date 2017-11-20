d3860b29cc4e1c52fe289b5e77c62db0f8aa61fb
Brice Tessier Huort <tessierhuort@gmail.com>
2017-11-20 12:15:50 +0100

    first commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,27 +1,26 @@
 const actions = {
-  'most-starred': require('./most-starred'),
-  'most-forked': require('./most-forked'),
-  'show-languages': require('./show-languages'),
-  'laugh': require('./laugh'),
+	'most-starred': require('./most-starred'),
+	'most-forked': require('./most-forked'),
+	'show-languages': require('./show-languages')
 }
 
 export default async function handleAction(res, payload) {
-  const currentAction = res.action && res.action.slug
-  console.log(currentAction)
-  let replies = []
-  if (actions[currentAction]) {
-    console.log('Enter action')
-    replies = await actions[currentAction].default(res, payload)
-  } else if (res.reply()) {
-    replies.push({
-      type: 'text',
-      content: res.reply(),
-    })
-  } else {
-    replies.push({
-      type: 'text',
-      content: 'Sorry I did not understand',
-    })
-  }
-  return replies
+	const currentAction = res.action && res.action.slug
+	console.log(currentAction)
+	let replies = []
+	if (actions[currentAction]) {
+		console.log('Enter action')
+		replies = await actions[currentAction].default(res, payload)
+	} else if (res.reply()) {
+		replies.push({
+			type: 'text',
+			content: res.reply()
+		})
+	} else {
+		replies.push({
+			type: 'text',
+			content: 'Sorry I did not understand'
+		})
+	}
+	return replies
 }
